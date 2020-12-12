30c3ddef8394b4ec314bfb68c8592380123f9e29
Morten <mtnielsen89@gmail.com>
2020-12-12 16:15:53 +0100

    Added manage user endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,10 +8,17 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse("user:create")
 TOKEN_URL = reverse("user:token")
+ME_URL = reverse("user:me")
+
 PAYLOAD = {
     "email": "test@test.com",
     "password": "minpass123",
     "name": "Testing"
+}
+PAYLOAD_UPDATE = {
+    "email": "test_update@test.com",
+    "password": "minpass1234",
+    "name": "Testing Update"
 }
 PAYLOAD_WRONG = {
     "email": "test@test.com",
@@ -85,4 +92,54 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URL, PAYLOAD_WRONG)
 
         self.assertNotIn("token", res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unautorized(self):
+        """Test that authentication is required for users"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test API requests that require authentication"""
+
+    def setUp(self):
+        self.user = create_user(**PAYLOAD)
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrive_profile_sucess(self):
+        """Test retrieving profile for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            "name": self.user.name,
+            "email": self.user.email
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that POST is not allowed on the me URL"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user"""
+        res = self.client.patch(ME_URL, PAYLOAD_UPDATE)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, PAYLOAD_UPDATE["name"])
+        self.assertTrue(self.user.check_password(PAYLOAD_UPDATE["password"]))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_password_too_short_update(self):
+        """Test that the password must be at least 10 characters on update"""
+        res = self.client.patch(ME_URL, PAYLOAD_WRONG)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        res = self.client.put(ME_URL, PAYLOAD_WRONG)
+
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
