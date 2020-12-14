51f353c8e80f3b7128c844c846d763e7e51d589d
Morten <mtnielsen89@gmail.com>
2020-12-14 09:16:21 +0100

    Added image to recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,9 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 import app.utils as utils
 
@@ -49,3 +53,13 @@ class ModelTests(TestCase):
         """Test the recipe string represention"""
         recipe = utils.create_recipe(self.user)
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch("uuid.uuid4")
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test that image is saved in the correct location"""
+        uuid = "test-uuid"
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, "myimage.jpg")
+
+        exp_path = f"uploads/recipe/{uuid}.jpg"
+        self.assertEqual(file_path, exp_path)
